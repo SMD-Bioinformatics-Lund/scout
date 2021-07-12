ebd316d2d99f5e90cfad1fb91b79404ff16eb570
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2021-07-12 09:20:00 +0200

    Patch chanjo install in scout setup.py

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
     include_package_data=True,
     extras_require={
         "coverage": [
+            "chanjo@git+https://git@github.com/Clinical-Genomics/chanjo.git@4.6.1",
             "chanjo-report",
             "pymysql",
         ],
